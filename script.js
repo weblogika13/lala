61b8140cc9b5a8ceda1629d996e3974645d97d42
oleksander.chicha <weblogika13@gmail.com>
2026-02-07 12:48:37 +0100

    зробили перекладаць

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.querySelector('#input')
-    const  = document.querySelector('#input')
+    const outputText = document.querySelector('#output')
+    const translateBtn = document.querySelector('#output')
 
 
+
+    async function translateText() {
+        const text = inputText.value.trim();
+        const safeText = encodeURIComponent(text);
+        const email = 'weblogika13@gmail.com'
+        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+
+        const.log(data);
+        outputText.textContent = data.responseData.translateText
+
+    }
+
+translateBtn.addEventListener('click', translateText);
 
 
     
