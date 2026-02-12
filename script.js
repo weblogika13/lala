0a304908099f817b4f67b32a57e66e407865d9dc
oleksander.chicha <weblogika13@gmail.com>
2026-02-12 15:28:09 +0100

    зроби текс введіть текст для перекладу

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.querySelector('#input')
     const outputText = document.querySelector('#output')
-    const translateBtn = document.querySelector('#output')
+    const translateBtn = document.querySelector('#translateBtn')
 
 
 
     async function translateText() {
         const text = inputText.value.trim();
+         if (text == '' ){
+            outputText.textContent = 'введіть текст для перекладу ';
+            return;
+         }
+
+
         const safeText = encodeURIComponent(text);
         const email = 'weblogika13@gmail.com'
         const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
@@ -17,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         console.log(data);
-        outputText.textContent = data.responseData.translateText
+        outputText.textContent = data.responseData.translatedText
 
     }
 
